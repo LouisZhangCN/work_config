8a55271bc6487eb1dab346c538120dcfbd8a30e1
Louis Zhang <lolkokpop@126.com>
2018-03-20 22:15:54 +0800

    coding work_env.sh.
    Signed-off-by: Louis Zhang <lolkokpop@126.com>

--- work_env.sh
+++ work_env.sh
@@ -1,3 +1,23 @@
 #!/bin/sh
+### Init one time, for personal work environment build.
 mkdir task src doc note tmp
 mkdir task/buglist task/tasklist
+
+
+
+
+### task
+cd task/buglist
+echo "bug information, at most 5 character:"
+read bugname
+
+mkdir bug_$bugname src note.md log
+
+echo "Do you have related source code:[Y/n]"
+read confirm
+echo $confirm
+if [ "${confirm}" == "y" ] || [ "${confirm}" == "Y" ] || [ "${confirm}" == " " ]; then
+    ln -sf task/src src/code
+else
+    mkdir src/code
+fi
